9f9b675c98f795ac8f88e7a71e51804a2ed83439
Eric Elliott <support@paralleldrive.com>
2017-01-22 17:36:52 -0700

    [Fixes #4] Errors thrown from onCancel() are swallowed.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-var speculation = function speculation (fn, cancel) {
+var speculation = function speculation (fn) {
+  var cancel = (
+    arguments.length > 1 &&
+    arguments[1] !== undefined
+  ) ? arguments[1] : Promise.reject('Cancelled');
+
   return new Promise(function (resolve, reject) {
     var noop = function noop () {};
 
     var handleCancel = function handleCancel (onCancel) {
-      return cancel.then(onCancel, noop);
+      return cancel.then(
+        onCancel,
+        noop
+      ).catch(function (e) {
+        return reject(e);
+      });
     };
 
     return fn(resolve, reject, handleCancel);
